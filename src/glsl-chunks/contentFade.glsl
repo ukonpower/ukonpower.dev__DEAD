a47ed8dc14823aa6163875c2efe6def6b7d19b3b
ukonpower <ore.ukonpower@gmail.com>
2022-05-29 13:38:10 +0900

    dom wip

--- src/glsl-chunks/contentFade.glsl
+++ src/glsl-chunks/contentFade.glsl
@@ -1,6 +1,6 @@
 void contentFade(in float visibility, sampler2D noiseTex, vec2 uv, inout vec3 color ) {
 
-	vec4 noise = texture2D( noiseTex, uv * 0.8 );
+	vec4 noise = texture2D( noiseTex, uv * 0.5 );
 
 	float v = smoothstep( 0.0, 1.0, -noise.x * 1.4 + visibility * 1.2 );
 	color += 1.0 - smoothstep( 0.0, 0.003, v ); 
